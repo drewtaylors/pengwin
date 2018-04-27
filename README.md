921745ae8ce0c8dad47a24a91a786b59b315f31a
Drew Taylor <drewtaylor888@gmail.com>
2018-04-26 20:05:57 -0700

    Initialized main bot.py file and started actions.py

---
+++ README.md
@@ -0,0 +1,6 @@
+Pengwin - Version 0.1.1
+Created by Drew Taylor
+
+Files:
+bot.py
+actions.py
---
+++ actions.py
@@ -0,0 +1,7 @@
+"""
+
+action.py - Drew Taylor - 4.26.18
+
+Module which stores all function logic for Pengwin's commands.abs
+
+"""
